31207f6b1d92d17f983004004c2e17263d776c07
minaiDocus <32548075+minaiDocus@users.noreply.github.com>
2018-02-01 17:04:47 +0300

    Add config file example
    Configuration file

---
+++ Config.js
@@ -0,0 +1,13 @@
+const Config = {
+  keydb: ###db key###,
+  version: "1.11",
+  platform: "android",
+
+  #### SERVER ACCESS #####
+  http_host: "http://localhost:3000/",  
+  server:"local, staging or production",
+  user:"##SERVER USER##",
+  pass:"##SERVER PASS##",
+}
+
+export default Config;
