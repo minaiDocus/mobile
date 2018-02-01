3d4193171dbb60ef0a12ef1953b803108883a1ee
Mina Rakotonirainy <mina@idocus.com>
2018-02-01 16:01:20 +0300

    Create branch master

---
+++ android/app/src/main/java/com/idocus/MainApplication.java
@@ -0,0 +1,61 @@
+package com.idocus;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
+import com.evollu.react.fcm.FIRMessagingPackage;
+import io.realm.react.RealmReactPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import org.wonday.pdf.RCTPdfView;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new SvgPackage(),
+            new PickerPackage(),
+            new FIRMessagingPackage(),
+            new RealmReactPackage(),
+            new SplashScreenReactPackage(),
+            new RCTPdfView(),
+            new LinearGradientPackage(),
+            new RNFetchBlobPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
